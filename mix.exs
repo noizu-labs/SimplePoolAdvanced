ab3169a3cfe682f2e67d906c0919d4e6ac51b51d
Keith Brings <keith.brings@noizu.com>
2022-04-19 14:10:58 +0700

    Incrementing version

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Noizu.AdvancedPool.Mixfile do
 
   def project do
     [app: :noizu_advanced_pool,
-      version: "2.1.3",
+      version: "2.1.4",
       elixir: "~> 1.9",
       package: package(),
       deps: deps(),
